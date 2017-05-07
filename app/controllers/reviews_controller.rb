4d5d359ba8b2c1be913b9a94426750c3e3c92b7e
jennafrancis <jef399@nyu.edu>
2017-05-07 19:17:55 -0400

    ReviewsController actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    review = Review.new(review_params)
+    review.user = current_user
+    if review.save
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content, :group_class_id)
+  end
 end
