27302993811b5540d3f6f46068787823f6940dff
jennafrancis <jef399@nyu.edu>
2017-05-07 18:01:20 -0400

    attempt at current_city logic

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,13 +3,13 @@ class CitiesController < ApplicationController
   end
 
   def create
-    # city = City.find()
-    redirect_to city_path(params[:studios][:city_id])
+    current_city(params[:studios][:city_id])
+    redirect_to city_path(current_city)
   end
 
   def show
     @city = City.find(params[:id])
-    @studios = Studio.where('city_id = ?', @city.id)
+    @studios = Studio.where('city_id = ?', current_city.id)
     #separate studio db logic to studio controller
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  # def current_city
+  #   @current_city ||= City.find(city)
+  # end
 end
