2e7ecc4b5328053c99ad28d63d246df40b05ea40
jennafrancis <jef399@nyu.edu>
2017-05-08 17:52:44 -0400

    start review form

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :group_class
+
+  validates_presence_of :content, :user_id, :group_class_id
+  validates_inclusion_of :rating, :in => 1..5
 end
