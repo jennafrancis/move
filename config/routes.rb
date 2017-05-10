111323e11a30e84293ed6632c232820d673c17b2
jennafrancis <jef399@nyu.edu>
2017-05-10 12:29:06 -0400

    nest review routes under group class

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   root 'cities#index'
   devise_for :users
   resources :categories, only: :show
-  resources :reviews
   resources :cities, only: [:index, :show]
   post '/cities', to: 'cities#set_city'
-  resources :group_classes, only: :show# nest routes within studio
+  resources :group_classes, only: :show do
+    resources :reviews
+  end
   resources :studios, only: :show
   resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
