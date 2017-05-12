2aff7391ffb18f6b420b53289a8b103560a0bb28
jennafrancis <jef399@nyu.edu>
2017-05-12 14:52:47 -0400

    omniauth w facebook working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,4 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
-  # OmniAuth.config.on_failure do |env|
-  #   [200, {}, [env['omniauth.error'].inspect]]
-  # end
 end
