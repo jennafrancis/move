83fa4d759487a50e33c8596fce0958aa66d14d96
jennafrancis <jef399@nyu.edu>
2017-05-12 11:32:16 -0400

    omniauth for facebook -- not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'cities#index'
-  devise_for :users
-  resources :categories, only: :show
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :cities, only: [:index, :show]
   post '/cities', to: 'cities#set_city'
   resources :group_classes, only: :show
   resources :reviews, only: [:new, :create, :show, :destroy]
   resources :studios, only: :show
   resources :users, only: :show
+  # get '/auth/facebook/callback' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
