793b2523d88ad9f7ca131afd56800983bc710bdd
jennafrancis <jef399@nyu.edu>
2017-05-15 14:52:22 -0400

    fix for review form bug creating invalid classes

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,7 +6,9 @@ class Review < ApplicationRecord
   validates_inclusion_of :rating, :in => 1..5
 
   def group_class_attributes=(group_class_attributes)
-    gc = GroupClass.find_or_create_by(group_class_attributes)
-    self.group_class = gc
+    if !group_class_attributes[:name].blank? && !group_class_attributes[:duration].blank?
+      gc = GroupClass.find_or_create_by(group_class_attributes)
+      self.group_class = gc
+    end
   end
 end
