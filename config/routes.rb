f3f06ca4099099555a11701add0f741975fef9f4
jennafrancis <jef399@nyu.edu>
2017-05-21 13:45:28 -0400

    nested resource for GroupClass in Studio

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :cities, only: [:index, :show]
   post '/cities', to: 'cities#set_city'
-  resources :group_classes, only: :show
+  # resources :group_classes, only: :show
   resources :reviews, only: [:new, :create, :show, :update, :destroy]
-  resources :studios, only: :show
+  resources :studios, only: :show do
+    resources :group_classes, only: :show
+  end
   resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
