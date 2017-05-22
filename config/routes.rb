5d1e5ab59268029cf6523a9a70406d487c69b5ba
jennafrancis <jef399@nyu.edu>
2017-05-22 11:42:09 -0400

    fix nested routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :cities, only: [:index, :show]
   post '/cities', to: 'cities#set_city'
-  # resources :group_classes, only: :show
+  resources :group_classes, only: :show
   resources :reviews, only: [:new, :create, :show, :update, :destroy]
   resources :studios, only: :show do
     resources :group_classes, only: :show
