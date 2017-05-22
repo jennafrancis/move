88cacf94a0b690e1a67b7b29b49fb3552cc811cb
jennafrancis <jef399@nyu.edu>
2017-05-22 18:08:11 -0400

    routing to add categories to a group class

--- app/controllers/group_classes_controller.rb
+++ app/controllers/group_classes_controller.rb
@@ -1,5 +1,18 @@
 class GroupClassesController < ApplicationController
+  before_action :set_group_class
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_group_class
     @gc = GroupClass.find(params[:id])
   end
 end
