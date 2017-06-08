45ff31509dc9f84039b4748dd836acc5b51f2134
jennafrancis <jef399@nyu.edu>
2017-06-08 10:56:49 -0400

    Add group class form independent of review creation, add validation for GroupClass inputs

--- app/models/group_class.rb
+++ app/models/group_class.rb
@@ -4,6 +4,7 @@ class GroupClass < ApplicationRecord
   has_many :category_group_classes
   has_many :categories, through: :category_group_classes
 
+  validates_presence_of :name, :duration
   validates :duration, numericality: { less_than: 360 }
 
   accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
