5cde0b4e0a65c93816760d4b518a1bf1aae3e855
jennafrancis <jef399@nyu.edu>
2017-06-21 17:09:46 -0400

    ajax post request appending properly - submit button bug to be fixed

--- app/assets/javascripts/group_classes.js
+++ app/assets/javascripts/group_classes.js
@@ -1,26 +1,15 @@
 $(function(){
   $("#new_review").on('submit', function(e) {
 
-    url = this.action
-
-    data = {
-      'authenticity_token' :$("input[name='authenticity_token']").val(),
-      'review' : {
-        'content' : $("#review_content"),
-        'rating' : $("#review_rating"),
-        'group_class_id' : $("#review_group_class_id"),
-        'user_id' : $("#review_user_id")
-      }
-    }
+    var $destination = $("div.gc_reviews")
 
     $.ajax({
       type: "POST",
-      url: url,
-      data: data,
+      url: this.action,
+      data: $(this).serialize(),
       success: function(response){
         $("#review_content").val("")
         $("#review_rating").val("")
-        var $destination = $("div.gc_reviews p")
         $destination.append(response);
       }
     });
