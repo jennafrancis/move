d410a538942613018ca0f5fe4c7cd0a978c6e669
jennafrancis <jef399@nyu.edu>
2017-06-21 20:21:17 -0400

    refactor to use jQuery post method

--- app/assets/javascripts/group_classes.js
+++ app/assets/javascripts/group_classes.js
@@ -1,18 +1,14 @@
 $(function(){
   $("#new_review").on('submit', function(e) {
-
+    var url = this.action
+    var data = $(this).serialize()
     var $destination = $("div.gc_reviews")
 
-    $.ajax({
-      type: "POST",
-      url: this.action,
-      data: $(this).serialize(),
-      success: function(response){
-        $("#review_content").val("")
-        $("#review_rating").val("")
-        $destination.append(response);
-      }
-    });
+    $.post(url, data, function(response){
+      $("#review_content").val("")
+      $("#review_rating").val("")
+      $destination.append(response);
+    })
 
     e.preventDefault();
   });
