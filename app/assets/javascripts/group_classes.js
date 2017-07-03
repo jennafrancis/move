ca277be9b105774db35de3a79662302a460f0f33
jennafrancis <jef399@nyu.edu>
2017-07-03 09:24:38 -0400

    add ability to click 'next' on show page

--- app/assets/javascripts/group_classes.js
+++ app/assets/javascripts/group_classes.js
@@ -7,15 +7,40 @@ $(function() {
     var source = document.getElementById("gc-show-template").innerHTML
     var template = Handlebars.compile(source);
 
-
     $.get(url, function(response){
       var result = template(response)
       var $destination = $("#main")
       $destination.empty()
       $destination.append(result)
 
+      attachNext()
       console.log(response)
     });
     e.preventDefault()
   });
 });
+
+function attachNext() {
+  alert("attached")
+  $(".js-next").click(function(e) {
+    e.preventDefault();
+    var studio_id = $(this).data("studio")
+    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+
+    var url = "/studios/" + studio_id + "/group_classes/" + nextId
+
+
+    var source = document.getElementById("gc-show-template").innerHTML
+    var template = Handlebars.compile(source);
+
+    $.get(url, function(response){
+      var result = template(response)
+      var $destination = $("#main")
+      $destination.empty()
+      $destination.append(result)
+      // re-set the id to current on the link
+      $(".js-next").attr("data-id", response["id"]);
+      attachNext()
+    });
+  });
+};
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -19,3 +19,16 @@ $(function(){
     });
   });
 });
+
+
+// <strong>Add a review:</strong>
+// <form class="new_review" id="new_review" action="/studios/1/group_classes/1" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="iVQHxS1A4KunHg9SLKaMbsDmfkyLmwA8HSA9TvVLgloNIazsmkxD/qu0yNnBZ+h/lHD1NamOVKRu4xNOWuxmIA==" />
+//   <label for="review_Rate this class (1-5)">Rate this class (1-5)</label>
+//   <input min="1" max="5" type="number" name="review[rating]" id="review_rating" /><br>
+//   <label for="review_What did you think">What did you think?</label>
+//   <textarea name="review[content]" id="review_content">
+// </textarea><br>
+//   <input value="1" type="hidden" name="review[group_class_id]" id="review_group_class_id" />
+//   <input value="2" type="hidden" name="review[user_id]" id="review_user_id" />
+//   <input type="submit" name="commit" value="Create Review" data-disable-with="Create Review" /><br>
+// </form>
