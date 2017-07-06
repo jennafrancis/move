011f9c2f50870b1f6fc782ed15aa80fc2121219c
jennafrancis <jef399@nyu.edu>
2017-07-06 09:55:02 -0400

    user review index bug fix

--- app/assets/javascripts/ausers.js
+++ app/assets/javascripts/ausers.js
@@ -1,5 +1,6 @@
 $(function(){
   $("a.load_reviews").click(function(e) {
+    e.preventDefault();
 
     $.get(this.href).done(function(json){
       var $list = $("div.reviews")
@@ -9,7 +10,5 @@ $(function(){
         $list.append(review.group_class.name + ' at ' + review.group_class.studio.name + '<br>' + review.rating + '/5 "' + review.content + '"<br></br>')
       });
     });
-
-    e.preventDefault();
   });
 });
