dacb0cd13263133fab6857d0c10cba4347101d40
Jenna Francis <jef399@nyu.edu>
2017-08-01 12:25:22 -0400

    Revert "Bootstrap theme"

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,9 +2,9 @@ class CitiesController < ApplicationController
   def index
   end
 
-  def show
+  def set_city
     session[:city_id].clear if session[:city_id]
-    session[:city_id] = params[:id]
+    session[:city_id] = params[:studios][:city_id]
     redirect_to studios_path
   end
 end
